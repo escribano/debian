fbee28f823bdae731c1929aa32f25c96472ab978
Ademir Escribano <ademir.escribano@gmail.com>
2014-10-18 10:48:43 -0300

    fix

---
+++ boot.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# wget https://github.com/escribano/habitatione/shell/debian/boot.sh
+# curl http://npmjs.org/install.sh | sh
+#
+# wget https://github.com/escribano/habitatione/raw/master/shell/debian/boot.sh
+
+function boot.git () {
+  git add .
+  git status
+  #git commit -am 'fix'
+  git commit -m 'server test'
+  git push origin master
+}
+
+function sudoi () {
+  sudo -i
+}
+
+function intall.packages () {
+  apt-get update && apt-get upgrade -y
+  apt-get install build-essential curl git unzip -y
+}
+
+function master.users () {
+  #ademir ALL=(ALL) NOPASSWD:ALL
+  #/etc/sudoers.d/master.users
+  useradd -m ademir -s /bin/bash
+  passwd ademir
+  gedulah
+
+  echo "ademir ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/master.users
+  mode 0440
+}
