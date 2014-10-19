ed51ce8d069dff3f9b2e384a81afe80f6b5ef5ba
Ademir Escribano <ademir.escribano@gmail.com>
2014-10-19 15:34:49 -0200

    fix

--- lib/sein.sh
+++ lib/sein.sh
@@ -1,11 +1,15 @@
 function install.sein () {
-  DAEMON_INSTALL_DIR="/opt/hab/daemon"
+  DAEMON_INSTALL_DIR="/opt/hab/daemon/sein"
   DAEMON_CONFIG_DIR="/etc/sein/"
-  sudo rm -rf $DAEMON_INSTALL_DIR
-  sudo mkdir -p $DAEMON_INSTALL_DIR
-  sudo rm -rf $DAEMON_CONFIG_DIR
-  sudo mkdir -p $DAEMON_CONFIG_DIR
-  sudo cp /tmp/sein $DAEMON_INSTALL_DIR/
-  sudo cp /tmp/config.toml $DAEMON_CONFIG_DIR/
+  sudo rm -rf $DAEMON_INSTALL_DIR && sudo mkdir -p $DAEMON_INSTALL_DIR
+  sudo rm -rf $DAEMON_CONFIG_DIR && sudo mkdir -p $DAEMON_CONFIG_DIR
+  #sudo cp /tmp/sein $DAEMON_INSTALL_DIR/
+  sudo tar xzvf /tmp/sein.tar.gz -C $INSTALL_DIR
+  # sudo ln -f -s $INSTALL_DIR/config/hab.debian.nginx.conf /etc/nginx/sites-enabled/hab
+  #sudo /etc/init.d/nginx reload
+  sudo cp $INSTALL_DIR/config.prd.toml $DAEMON_CONFIG_DIR/config.toml
 }
 
+function run.sein () {
+  /opt/hab/daemon/sein
+}
